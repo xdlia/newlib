42d2f0cc86de98f0f63eee2eb2585113f3d9f042
fincs <fincs@devkitpro.org>
2019-03-08 19:20:16 +0100

    Add basic pthread support to libsysbase with corresponding syscalls

--- libgloss/libsysbase/syscall_support.c
+++ libgloss/libsysbase/syscall_support.c
@@ -1,33 +1,51 @@
+#include <errno.h>
 #include <sys/iosupport.h>
+#include <sys/time.h>
 
-//---------------------------------------------------------------------------------
-__syscalls_t __syscalls = {
-//---------------------------------------------------------------------------------
-	NULL,	// sbrk
-	NULL,	// exit
-	NULL,	// gettod_r
-	NULL,	// lock_init
-	NULL,	// lock_acquire
-	NULL,	// lock_try_acquire
-	NULL,	// lock_release
-	NULL,	// lock_close
-	NULL,	// lock_init_recursive
-	NULL,	// lock_acquire_recursive
-	NULL,	// lock_try_acquire_recursive
-	NULL,	// lock_release_recursive
-	NULL,	// lock_close_recursive
-	NULL,	// __getreent
-	NULL,	// clock_gettime
-	NULL,	// clock_settime
-	NULL,	// clock_getres
-	NULL,	// nanosleep
-};
+__syscalls_t __syscalls;
+
+static int
+timespec_subtract(struct timespec x, struct timespec y, struct timespec *__restrict result)
+{
+	// Perform the carry for the later subtraction by updating y
+	if (x.tv_nsec < y.tv_nsec) {
+		int seconds = (y.tv_nsec - x.tv_nsec) / 1000000000 + 1;
+		y.tv_nsec -= 1000000000 * seconds;
+		y.tv_sec += seconds;
+	}
+	if (x.tv_nsec - y.tv_nsec > 1000000000) {
+		int seconds = (x.tv_nsec - y.tv_nsec) / 1000000000;
+		y.tv_nsec += 1000000000 * seconds;
+		y.tv_sec -= seconds;
+	}
+
+	// Compute the time remaining to wait
+	result->tv_sec = x.tv_sec - y.tv_sec;
+	result->tv_nsec = x.tv_nsec - y.tv_nsec;
+
+	// Return true if result is negative
+	return x.tv_sec < y.tv_sec;
+}
+
+__uint64_t
+timespec2nsec(const struct timespec *__restrict ts)
+{
+	return (__uint64_t)ts->tv_sec * 1000000000 + ts->tv_nsec;
+}
+
+__uint64_t
+abstimespec2nsec(__clockid_t clock_id, const struct timespec *__restrict ts)
+{
+	struct timespec now, diff;
+	clock_gettime(clock_id, &now);
+	if (timespec_subtract(*ts, now, &diff))
+		return 0;
+	return timespec2nsec(&diff);
+}
 
 void __libc_lock_init(_LOCK_T *lock) {
 
-	if ( __syscalls.lock_init ) {
-		__syscalls.lock_init(lock);
-	}
+	*lock = __LOCK_INITIALIZER;
 
 }
 
@@ -56,18 +74,13 @@ void __libc_lock_release(_LOCK_T *lock ) {
 
 void __libc_lock_close(_LOCK_T *lock ) {
 
-	if ( __syscalls.lock_close) {
-		__syscalls.lock_close(lock);
-	}
 }
 
 
 
 void __libc_lock_init_recursive(_LOCK_RECURSIVE_T *lock) {
 
-	if ( __syscalls.lock_init_recursive ) {
-		__syscalls.lock_init_recursive(lock);
-	}
+	*lock = __LOCK_INITIALIZER_RECURSIVE;
 
 }
 
@@ -96,8 +109,52 @@ void __libc_lock_release_recursive(_LOCK_RECURSIVE_T *lock ) {
 
 void __libc_lock_close_recursive(_LOCK_RECURSIVE_T *lock ) {
 
-	if ( __syscalls.lock_close_recursive) {
-		__syscalls.lock_close_recursive(lock);
+}
+
+
+int __libc_cond_init(_COND_T *cond) {
+
+	*cond = __COND_INITIALIZER;
+
+}
+
+int __libc_cond_signal(_COND_T *cond) {
+
+	if ( __syscalls.cond_signal) {
+		return __syscalls.cond_signal(cond);
 	}
+
+	return ENOSYS;
+
+}
+
+int __libc_cond_broadcast(_COND_T *cond) {
+
+	if ( __syscalls.cond_broadcast) {
+		return __syscalls.cond_broadcast(cond);
+	}
+
+	return ENOSYS;
+
+}
+
+int __libc_cond_wait(_COND_T *cond, _LOCK_T *lock, uint64_t timeout_ns) {
+
+	if ( __syscalls.cond_wait) {
+		return __syscalls.cond_wait(cond, lock, timeout_ns);
+	}
+
+	return ENOSYS;
+
+}
+
+int __libc_cond_wait_recursive(_COND_T *cond, _LOCK_RECURSIVE_T *lock, uint64_t timeout_ns) {
+
+	if ( __syscalls.cond_wait_recursive) {
+		return __syscalls.cond_wait_recursive(cond, lock, timeout_ns);
+	}
+
+	return ENOSYS;
+
 }
 
